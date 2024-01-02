7e83f5acb7863e5a75070d51bbc3e7c93e124b46
kuba <jwolansk@gmail.com>
2024-01-02 16:55:06 +0100

    publish dynamic library

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "RxComposableArchitecture",
+            type: .dynamic,
             targets: ["RxComposableArchitecture"]),
     ],
     dependencies: [

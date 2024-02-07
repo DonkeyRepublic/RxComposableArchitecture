158e021307e5ccf7a4f51ebc4f86cd3ad49d784a
kuba <jwolansk@gmail.com>
2024-02-07 10:29:18 +0100

    static build

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "RxComposableArchitecture",
-            type: .dynamic,
+            type: .static,
             targets: ["RxComposableArchitecture"]),
     ],
     dependencies: [
